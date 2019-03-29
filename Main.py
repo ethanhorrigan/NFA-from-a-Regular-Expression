5e68eafd539414c97a3be8a69da45e4daaec14a9
EthanHorrigan <ethanhorr@gmail.com>
2019-03-29 14:21:11 +0000

    Removed Shunt.py and merged into one class
    Removed the Shunt.py which contained the Shunting Yard Algorithm, I then moved this algorithm into my Main class
    
    Various Comments Added

--- Main.py
+++ Main.py
@@ -1,2 +1,41 @@
-print("This line will be printed.")
+# Ethan Horrigan
+# G00350712
+# Program to build a non-deterministic ﬁnite automaton (NFA) from a regular expression
 
+# Shunting Yard Algorithm
+
+def shunt(infix):
+ """Converts an infix expression to a postfix expression
+    using the Shunting-yard algorithm"""
+    
+    # Specials Dictionary
+    specials = {'*': 50, '.': 40, '|': 30}
+
+    # pofix & stack string (empty)
+    pofix = ""
+    stack = ""
+
+    #loop infix string
+    for c in infix:
+        if c == '(':
+            stack = stack + c
+        elif c == ')':
+            while stack[-1] != '(':
+                pofix = pofix + stack[-1]
+                stack = stack[:-1]
+            stack = stack[:-1] 
+        elif c in specials:
+            while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
+                pofix = pofix + stack[-1]
+                stack = stack[:-1]  
+            stack = stack + c
+        else:
+            pofix = pofix + c
+
+    while stack:
+        pofix = pofix + stack[-1]
+        stack = stack[:-1]  
+
+    return pofix
+
+print(shunt("(a.b)|(c*.d)"))
